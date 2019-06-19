fe6a776ef1afefdaaedd123d53bb05330e4615be
hxs <work_hxs@163.com>
2019-06-19 09:09:35 +0800

    更新gitignore文件

--- Example/Pods/Target Support Files/XSExtension/XSExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XSExtension : NSObject
-@end
-@implementation PodsDummy_XSExtension
-@end
--- Example/Pods/Target Support Files/XSUtil/XSUtil-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XSUtil : NSObject
-@end
-@implementation PodsDummy_XSUtil
-@end
